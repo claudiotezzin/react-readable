6bbc6d790409763c5ae49fc444a73deddbc51fc7
Claudio Tezzin Jeremias <claudio.jeremias@clansoft.com.br>
2018-05-09 13:00:10 -0300

    Initializing project.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
-import logo from "logo.svg";
+import logo from "assets/images/readable.png";
+import "normalize.css";
+import "styles/vendors/css/Grid.css";
 import "styles/App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <header>
+        <img className="app-logo" src={logo} alt="Readable Logo" />
+      </header>
     );
   }
 }
